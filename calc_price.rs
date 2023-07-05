3f23ca6fe184b54a0814bc42573a6c1bf072686c
Kiwon Kwon <kkwon@uei.com>
2023-07-05 11:39:58 +0900

    어떤 컴퓨터 업체의 PC 가격은 98만 원이다. A 쇼핑몰에서는 배송비가 12,000원이고 20% 할인된 가격으로 판매하고 있다. B 쇼핑몰에서는 배송료가 무료지만 10% 할인된 가격으로 팔고 있다. 두 쇼핑몰 중 어느 쇼핑몰이 싼지 계산하시오

---
+++ calc_price.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let pc = 980000;
+    let delivery = 12000;
+
+    let a = pc*80/100+delivery;
+    let b = pc*90/100;
+
+    println!("A 쇼핑몰 = {}원",a);
+    println!("B 쇼핑몰 = {}원",b);
+}
