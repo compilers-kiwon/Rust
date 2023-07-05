b4aa18c33779da22272657543ac219bfceba31a4
Kiwon Kwon <kkwon@uei.com>
2023-07-05 12:34:54 +0900

    Simplify the code with using Closure

--- caeser_enc.rs
+++ caeser_enc.rs
@@ -1,20 +1,9 @@
 fn encrypt(text: &str, shift: i16) -> String {
-    let code_a = 'A' as i16;
-    let code_z = 'Z' as i16;
-
-    let mut result = String::new();
-
-    for ch in text.chars() {
-        let mut code = ch as i16;
-
-        if code_a <= code && code <= code_z {
-            code = (code-code_a+shift+26)%26+code_a;
-        }
-
-        result.push((code as u8) as char);
-    }
-
-    return  result;
+    let a = 'A' as i16;
+    let is_upper_alpha = |c| 'A' <= c && c <= 'Z';
+    let conv = |c| (((c-a+shift+26)%26+a) as u8) as char;
+    let enc = |c| if is_upper_alpha(c) {conv(c as i16)} else {c};
+    text.chars().map(|c| enc(c)).collect()
 }
 
 fn main() {
