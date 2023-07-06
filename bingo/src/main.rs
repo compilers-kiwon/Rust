7d73428174867796a4fbf0fdb067f3f54379dd19
Kiwon Kwon <kkwon@uei.com>
2023-07-06 14:43:52 +0900

    using a vector instead of a static array

--- bingo/src/main.rs
+++ bingo/src/main.rs
@@ -7,8 +7,8 @@ const JOKER: usize = 12;
 
 fn main() {
     // 1~75 array
-    let mut nums = [0;MAX_NUM];
-    for i in 1..75 { nums[i-1] = i;}
+    let mut nums = vec![];
+    for i in 1..MAX_NUM { nums.push(i);}
 
     // shuffle
     let mut rng = rand::thread_rng();
