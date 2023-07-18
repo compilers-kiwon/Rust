1f1323018367cdee35e3cdc8c6eca844a1db6e51
Kiwon Kwon <kkwon@uei.com>
2023-07-18 14:43:28 +0900

    Text 파일을 그대로 읽어서 출력하기

---
+++ read_file.rs
@@ -0,0 +1,21 @@
+use std::env;   // 명령줄 인수 취득용
+use std::fs;    // 파일 읽기용
+
+fn main() {
+    // 인수를 벡터로 취득
+    let args: Vec<String> = env::args().collect();
+
+    // 인수에 파일 이름을 지정했는가?
+    if args.len() < 2 {
+        println!("There is no file name.");
+        return;
+    }
+
+    // 두번째 인수 => 파일 이름
+    let filename = &args[1];
+
+    // 파일 읽기
+    let text = fs::read_to_string(filename).unwrap();
+
+    println!("{}", text);
+}
