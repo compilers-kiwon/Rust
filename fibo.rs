8638e8b4ff2b7485d75f06c68587a3ffb16c9ba4
Kiwon Kwon <kkwon@uei.com>
2023-07-19 15:27:32 +0900

    recursive call 을 이용하여 fibonacci 함수값 구하기

--- fibo.rs
+++ fibo.rs
@@ -1,15 +1,23 @@
+fn fibo(n:i64) -> i64 {
+    if n == 1 { return  0; }
+    if n == 2 { return  1; }
+    return  fibo(n-2)+fibo(n-1);
+}
+
+fn input(prompt: &str) -> i64 {
+    // 메세지 출력
+    println!("{}", prompt);
+    // 입력값을 가져옴
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).expect("잘못된 입력입니다.");
+    // 공백을 제거하고 숫자 값으로 변환
+    return  s.trim().parse().expect("숫자가 아닙니다.");
+}
+
 fn main() {
-    let mut a = 1;
-    let mut b = 1;
+    let n = input("1 부터 몇까지?");
 
-    println!("{}",a);
-    println!("{}",b);
-
-    for _ in 0..30 {
-        let cur = a+b;
-
-        println!("{}",cur);
-        a = b;
-        b = cur;
+    for i in 1..n+1 {
+        println!("fibo({}) = {}",i,fibo(i));
     }
 }
