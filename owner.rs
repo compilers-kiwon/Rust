2c68e8bc3a61592a8d93f97d1132a49525f36d4e
Kiwon Kwon <kkwon@uei.com>
2023-07-24 17:29:02 +0900

    역참조(dereference) 예제

--- owner.rs
+++ owner.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let mut msg = "건강한 신체에 건강한 정신이 깃든다.".to_string();
-    println!("{}",msg);
-    add_quote(&mut msg);
-    println!("{}",msg);
+    let mut v = 16;
+    x2(&mut v);
+    println!("{}",v);
 }
 
-fn add_quote(msg: &mut String) {
-    msg.insert(0,'"');
-    msg.push('"');
+fn x2(arg: &mut i32) {
+    *arg = (*arg)*2;
 }
