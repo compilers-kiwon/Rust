074a2d5c560c15f7a1d7c6ebf0b60684e1e7c014
Kiwon Kwon <kkwon@uei.com>
2023-07-24 17:16:55 +0900

    호출된 함수가 소유권을 return 하는 경우

--- owner.rs
+++ owner.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let g1 = 30;
-    let g2 = g1;
-    println!("g1={}, g2={}",g1,g2);
+    let mut g1 = "실수할 줄 아는 사람이 아름답다".to_string();
+    g1 = show_message(g1);
+    println!("g1의 소유권이 유효함, {}",g1);
+}
 
-    let s1 = String::from("온화한 마음은 몸에 좋다");
-    let s2 = s1.clone();
-
-    println!("{}",s1);
-    println!("{}",s2);
+fn show_message(message: String) -> String {
+    println!("{}",message);
+    return message;
 }
