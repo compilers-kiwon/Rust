bc6f23b66e19b1c2543f5c7ddc3c200cf94efd76
Kiwon Kwon <kkwon@uei.com>
2023-07-24 17:20:28 +0900

    호출된 함수에게 소유권을 빌려주는 경우(참조)

--- owner.rs
+++ owner.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let mut g1 = "실수할 줄 아는 사람이 아름답다".to_string();
-    g1 = show_message(g1);
+    let g1 = "실수할 줄 아는 사람이 아름답다".to_string();
+    show_message(&g1);
     println!("g1의 소유권이 유효함, {}",g1);
 }
 
-fn show_message(message: String) -> String {
+fn show_message(message: &String) {
     println!("{}",message);
-    return message;
 }
