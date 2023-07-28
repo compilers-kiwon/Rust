44c3e26008db3238799da2c8873640970e4d6212
Kiwon Kwon <compilers@naver.com>
2023-07-28 16:51:31 +0900

    구조체와 method() 를 이용하여 BIM 구하기

--- bmi.rs
+++ bmi.rs
@@ -5,70 +5,68 @@ struct BmiRange {
     label: String,  // 판정
 }
 
-fn main() {
-    // 키와 몸무게 입력
-    let height_cm = input("키(cm): ");
-    let weight = input("몸무게(kg): ");
-    
-    // BMI 계산
-    let height = height_cm / 100.0;
-    let bmi = weight / height.powf(2.0);
-    println!("BMI={:.1}", bmi);
-
-    // 비만도 판정표를 벡터 타입으로 생성
-    let bmi_table = vec![
-        BmiRange {
-            min: 0.0,
-            max: 18.5,
-            label: "저체중".to_string(),
-        },
-        BmiRange {
-            min: 18.5,
-            max: 23.0,
-            label: "정상".to_string(),
-        },
-        BmiRange {
-            min: 23.0,
-            max: 25.0,
-            label: "비만전단계".to_string(),
-        },
-        BmiRange {
-            min: 25.0,
-            max: 30.0,
-            label: "1단계 비만".to_string(),
-        },
-        BmiRange {
-            min: 30.0,
-            max: 35.0,
-            label: "2단계 비만".to_string(),
-        },
-        BmiRange {
-            min: 35.0,
-            max: 99.0,
-            label: "3단계 비만".to_string(),
-        },
-    ];
-
-    // 비만도 진단
-    let mut result = "판단 불가".to_string();
-
-    for range in bmi_table {
-        if range.min<=bmi && bmi<range.max {
-            result = range.label;
-            break;
-        }
+impl BmiRange {
+    // 생성자
+    fn new(min: f64, max: f64, label: &str) -> Self {
+        BmiRange {min, max, label: label.to_string(),}
     }
-
-    // 결과 표시
-    println!("비만도 = {}",result);
+    // 범위 안에 있는지 확인하는 함수
+    fn in_range(&self, v: f64) -> bool {
+        (self.min <= v) && (v < self.max)
+    }
 }
 
-fn input(prompt: &str) -> f64 {
-    // 메세지 출력
-    println!("{}", prompt);
-    // 입력값을 가져옴
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).expect("잘못된 입력입니다.");
-    // 공백을 제거하고 숫자 값으로 변환
-    return  s.trim().parse().expect("숫자가 아닙니다.");
+// 학생의 정보(키,몸무게,이름)를 저장할 구조체
+struct Body {
+    height: f64,    // cm
+    weight: f64,    // kg
+    name: String,   // 이름
+}
+
+impl Body {
+    // 생성자
+    fn new(height: f64, weight: f64, name: &str) -> Self {
+        Body {height, weight, name: name.to_string(),}
+    }
+    // BMI 계산
+    fn get_bmi(&self) -> f64 {
+        self.weight/(self.height/100.0).powf(2.0)
+    }
+    // 비만 판정 출력
+    fn print_result(&self, table: &Vec<BmiRange>) {
+        // BMI 구하기
+        let bmi = self.get_bmi();
+        let mut result = "계산 불가".to_string();
+
+        for range in table {
+            if range.in_range(bmi) {
+                result = range.label.clone();
+                break;
+            } 
+        }
+
+        println!("{}님, BMI = {:.1}, 결과 = {}", self.name, bmi, result);
+    }
+}
+
+fn main() {
+    // 비만도 판정표를 벡터 타입으로 생성
+    let bmi_table = vec![
+        BmiRange::new(0.0,18.5,"저체중"),
+        BmiRange::new(18.5,23.0,"정상"),
+        BmiRange::new(23.0,25.0,"비만전단계"),
+        BmiRange::new(25.0,30.0,"1단계 비만"),
+        BmiRange::new(30.0,35.0,"2단계 비만"),
+        BmiRange::new(35.0,99.0,"3단계 비만"),
+    ];
+
+    // 비만도를 진단할 학생들 정보
+    let sung = Body::new(163.0, 75.2, "성은");
+    let ga = Body::new(158.2, 55.0, "가빈");
+    let chae = Body::new(174.2, 54.2, "채연");
+
+    // 비만도 진단
+    sung.print_result(&bmi_table);
+    ga.print_result(&bmi_table);
+    chae.print_result(&bmi_table);
 }
