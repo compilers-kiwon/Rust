388327536ad15e09b4105063495c5a95ed361bb8
Kiwon Kwon <compilers@naver.com>
2023-07-31 17:39:00 +0900

    Match guard

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,11 +1,11 @@
 fn main() {
     for i in 1..=100 {
-        // 값을 match 로 분기. 튜플을 이용한다.
-        match (i%3, i%5) {
-            (0, 0) => println!("FizzBuzz"),
-            (0, _) => println!("Fizz"),
-            (_, 0) => println!("Buzz"),
-            _      => println!("{}", i)
-        }
+        let msg = match i {
+            n if n%15 == 0 => "FizzBuzz".to_string(),
+            n if n%3 == 0 => "Fizz".to_string(),
+            n if n%5 == 0 => "Buzz".to_string(),
+            _ => format!("{}", i)
+        };
+        println!("{}", msg);
     }
 }
