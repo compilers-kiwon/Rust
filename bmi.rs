211b9c437ae63a9ba3ce0fb8ae385854f0b7d0e2
Kiwon Kwon <compilers@naver.com>
2023-08-01 13:42:34 +0900

    체중을 블루투스 체중계로 부터 읽어 온다고 가정했을 때, 블루투스 체중계의 오작동으로 체중을 읽어 오지 못할 때의 예외 처리에 match guard 적용

--- bmi.rs
+++ bmi.rs
@@ -18,34 +18,43 @@ impl BmiRange {
 
 // 학생의 정보(키,몸무게,이름)를 저장할 구조체
 struct Body {
-    height: f64,    // cm
-    weight: f64,    // kg
-    name: String,   // 이름
+    height: f64,        // cm
+    weight: Option<f64>,// kg
+    name: String,       // 이름
 }
 
 impl Body {
     // 생성자
-    fn new(height: f64, weight: f64, name: &str) -> Self {
+    fn new(height: f64, weight: Option<f64>, name: &str) -> Self {
         Body {height, weight, name: name.to_string(),}
     }
     // BMI 계산
-    fn get_bmi(&self) -> f64 {
-        self.weight/(self.height/100.0).powf(2.0)
+    fn get_bmi(&self) -> Option<f64> {
+        match self.weight {
+            None => None,
+            Some(w) => Some(w/(self.height/100.0).powf(2.0))
+        }
     }
     // 비만 판정 출력
     fn print_result(&self, table: &Vec<BmiRange>) {
         // BMI 구하기
-        let bmi = self.get_bmi();
-        let mut result = "계산 불가".to_string();
+        let bmi: Option<f64> = self.get_bmi();
+        let result = match bmi {
+            None => "계산 불가".to_string(),
+            Some(b) => {
+                let mut ret_str = "".to_string();
+                for range in table {
+                    if range.in_range(b) {
+                        ret_str = range.label.clone();
+                        break;
+                    } 
+                }
+                ret_str
+            }
+        };
 
-        for range in table {
-            if range.in_range(bmi) {
-                result = range.label.clone();
-                break;
-            } 
-        }
-
-        println!("{}님, BMI = {:.1}, 결과 = {}", self.name, bmi, result);
+        println!("{}님, BMI = {:.1}, 결과 = {}", self.name, 
+                    match bmi { None=>0.0, Some(b)=>b }, result);
     }
 }
 
@@ -61,9 +70,9 @@ fn main() {
     ];
 
     // 비만도를 진단할 학생들 정보
-    let sung = Body::new(163.0, 75.2, "성은");
-    let ga = Body::new(158.2, 55.0, "가빈");
-    let chae = Body::new(174.2, 54.2, "채연");
+    let sung = Body::new(163.0, Some(75.2), "성은");
+    let ga = Body::new(158.2, Some(55.0), "가빈");
+    let chae = Body::new(174.2, None, "채연");
 
     // 비만도 진단
     sung.print_result(&bmi_table);
