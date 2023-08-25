7e6268ee52c77c866be93f9689bc14e2179574aa
Kiwon Kwon <compilers@naver.com>
2023-08-25 14:51:13 +0900

    열거형 enum 으로 Linked List 구현하기

--- linked_list.rs
+++ linked_list.rs
@@ -1,27 +1,32 @@
-// 단방향 연결 리스트
-pub struct Node {
-    data: i64,
-    link: Option<Box<Node>>
+// 열거형으로 Node 를 정의
+enum Node {
+    Empty,
+    Cons(i64, Box<Node>),
 }
 
-// 단방향 연결 리스트를 생성하는 함수
-fn node(v: i64, link: Option<Box<Node>>) -> Option<Box<Node>> {
-    Some(Box::new(Node{data: v, link: link}))
+// 열거형을 더 쉽게 사용할 수 있게 해주는 선언 및 함수
+use Node::{Empty, Cons};
+fn node(v: i64, link: Box<Node>) -> Box<Node> {
+    Box::new(Cons(v, link))
 }
 
 fn main() {
     // 단방향 연결 리스트 생성
-    let c = node(10, node(20, node(30, None))).unwrap();
+    let c = node(10, node(20, node(30, Box::new(Empty))));
 
     // 가장 앞에서부터 각 요소를 따라가며 값을 표시
-    let mut p = &c;
+    let mut ptr: &Box<Node> = &c;
 
     loop {
-        println!("{}", p.data);
-        // p가 다음 요소를 가리키도록 변경
-        match p.link {
-            None => break,
-            Some(ref link) => p = &link,
+        // &Box<Node> 에서 Node 를 꺼내와 출력
+        let cur_node: &Node = &**ptr;
+        
+        match cur_node {
+            Empty => break,
+            Cons(v, link) => {
+                println!("{}", v);
+                ptr = &link;
+            }
         }
     }
 }
