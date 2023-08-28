fa6ecb6aa3857d3e090077c36267822a839e9d47
Kiwon Kwon <compilers@naver.com>
2023-08-28 17:02:52 +0900

    값을 공유할 수 있는 Rc<T> 와 값 변경이 가능한 RefCell<T> 를 조합하여 여러 사용자를 지정하면서도 값을 변경할 수 있는 타입 지정하기

---
+++ rc_mod.rs
@@ -0,0 +1,13 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+fn main() {
+    // 힙 영역에 i32 가변성을 가진 i32 타입 값 1000 dmf wjwkd
+    let a = Rc::new(RefCell::new(1000));
+    // 참조 카운터 증가
+    let b = Rc::clone(&a);
+    // 값 변경 시도
+    *b.borrow_mut() += 100;
+    // 원래의 참조 값 확인
+    println!("{}", a.borrow());
+}
